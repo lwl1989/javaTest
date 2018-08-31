a6b1ed785f750ded11b4d72ee76d078ee209f267
李文龙 <wenlong11@staff.weibo.com>
2018-08-31 20:25:11 +0800

    obj

---
+++ src/Obj/TelPhone.java
@@ -0,0 +1,18 @@
+package Obj;
+
+public class TelPhone {
+
+    //属性 有什么
+    float screen;
+    float cpu;
+    float mem;
+
+    //方法 能干什么
+    void  call() {
+        System.out.println("能打电话");
+    }
+
+    void sendMessage() {
+        System.out.println("能发短信");
+    }
+}
