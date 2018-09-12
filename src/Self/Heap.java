f08dc6417999d5166fa83f58e2a564f38156bc8c
李文龙 <wenlong11@staff.weibo.com>
2018-09-12 12:48:28 +0800

    heap

--- src/Self/Heap.java
+++ src/Self/Heap.java
@@ -1,29 +1,61 @@
 package Self;
 
-import java.util.Arrays;
-
 /**
  * 堆排序
+ * 几个关键要素  
+ *       最后一个节点  arr.length - 1
+ *       任意一个节点的父亲节点   parent = (child - 1) / 2
+ *
+ *       整体思想，元素之需要和他的父亲元素进行比较
+ *       假如大于/小于父亲元素，则再次对比父亲的元素的父亲，以此类推
+ *       因此，比较的最大次数相当于二叉树的深度
+ *
+ *
+ *
+ *       最小堆 最小堆任何一个父节点的值，都小于等于它左右孩子节点的值。
+ *       最大堆 最大堆任何一个父节点的值，都大于等于它左右孩子节点的值。
+ *
+ *
+ *       二叉树公式  2^k-1 > len > 2^(k-1)-1
+ *       满二叉树长度（已知深度）  len = 2^k -1
+ *
+ *       最后一个节点 arr.length - 1
+ *       父亲节点  parent = (child - 1) / 2 && child > 0
+ *       度是深度的反转  假如深度为n 度为0的则是叶子节点 度为n-1的则是根节点
  */
 public class Heap {
     public static void main(String[] args) {
         int[] arr = {22,3,112,56,23,12,45,78,97,24,35};
 
-        int[] arr1 = buildMaxHeap(arr);
+        buildMaxHeap(arr);
         //int[] arr2 = buildMinHead(arr);
 
+        for(int v : arr) {
+            System.out.println(v);
+        }
+        System.out.println();
+        int[] arr1 = add(arr,1);
         for(int v : arr1) {
+            System.out.println(v);
+        }
+//        for(int v : arr) {
+//            System.out.println(v);
+//        }
+        System.out.println();
+        int[] arr2 = remove(arr1,3);
+        for(int v : arr2) {
             System.out.println(v);
         }
     }
 
-    public static int[] buildMaxHeap(int[] arr) {
+
+    public static void buildMaxHeap(int[] arr) {
         //任何一个二叉树的非叶子节点都是 arr.length/2  3 =》 3/2 => 1
         for (int i = arr.length/2; i>=0; i--) {
-            arr = downHeap(arr, i, arr.length-1);
+            downHeap(arr, i, arr.length-1);
         }
-
-        return  arr;
+        //java数组是引用传递
+     //   return  arr;
     }
 
 //    public static int[] buildMinHead(int[] arr) {
@@ -31,52 +63,88 @@ public class Heap {
 //    }
 //
     //从上往下寻址
-    public static int[] downHeap(int[] arr, int parentId, int len) {
-        int temp = arr[parentId];
-        int childId = 2*parentId+1; //反之求
+    public static void downHeap(int[] arr, int parentIndex, int len) {
+        int temp = arr[parentIndex];
+        int childIndex = 2*parentIndex+1; //反之求
 
-        while (childId < len) {
-            //假如孩子节点有右节点 并且 右节点小鱼左节点  节点转移到右节点位置
-            if(childId + 1 < len && arr[childId+1] < arr[childId]) {
-                childId ++;
+        while (childIndex < len) {
+            //
+            //      5            5>2 && 2<3 && 2<5  => index变成2的index
+            //    3    2                     比较节点的位置放入2  节点位置改为2的节点位置 并寻找是否有下一个子节点
+            //       6   4       5>3 && 4<6 && 4<5
+            //                               比较节点的位置放入4  节点位置改为4的节点位置 并寻找是否有下一个子节点
+            //假如孩子节点有右节点 并且 右节点小于左节点  节点转移到右节点位置
+            if(childIndex + 1 < len && arr[childIndex+1] < arr[childIndex]) {
+                childIndex ++;
             }
 
             //如果父节点 小于当前孩子节点  停止判断
-            if(temp < arr[childId]) {
+            if(temp < arr[childIndex]) {
                 break;
             }
 
-            arr[parentId] = arr[childId];
-            parentId = childId;
-            childId = 2*parentId+1;
+            arr[parentIndex] = arr[childIndex];
+            parentIndex = childIndex;
+            childIndex = 2*parentIndex+1;
         }
-        arr[parentId] = temp;
-        return arr;
+        arr[parentIndex] = temp;
+        //return arr;
     }
 
-    public static int[] upHeap(int[] arr) {
-        int childId = arr.length - 1; //最后一个节点的下标  比如 数组长度5  下标则为4
-        int parentId =  (childId-1)/2;  //它的父亲节点的下标  比如 数组长度5  下标为4  4-1 = 3 /2  =1
-        int temp = arr[childId];
+    public static void upHeap(int[] arr) {
+        int childIndex = arr.length - 1; //最后一个节点的下标  比如 数组长度5  下标则为4
+        int parentIndex =  (childIndex-1)/2;  //它的父亲节点的下标  比如 数组长度5  下标为4  4-1 = 3 /2  =1
+        int temp = arr[childIndex];
 
-        while (childId > 0 && temp < arr[parentId]) {
-            arr[childId] = arr[parentId];   //将大的元素下移 将小的元素上移动
-            childId = parentId;
-            parentId = (parentId-1)/2;
+        while (childIndex > 0 && temp < arr[parentIndex]) {
+            arr[childIndex] = arr[parentIndex];   //将大的元素下移 将小的元素上移动
+            childIndex = parentIndex;
+            parentIndex = (parentIndex-1)/2;
         }
-        arr[childId] = temp; // 最终要放的位置
+        arr[childIndex] = temp; // 最终要放的位置
 
+    }
+
+
+    public static int[] remove(int[] arr, int value) {
+        int parentIndex = -1;
+        for(int i = 0; i < arr.length; i ++) {
+            if(value == arr[i]) {
+                parentIndex = i;
+            }
+        }
+        if(parentIndex == -1) {
+            return arr;
+        }
+        //将最后一个元素填充到第一个元素
+        arr[parentIndex] = arr[arr.length-1];
+
+
+        downHeap(arr, 0, arr.length-1);
 
         return arr;
     }
+//
 
-//    public static int[] remove(int[] arr, int value) {
-//
-//    }
-//
-//    public static int[] add(int[] arr, int value) {
-//
-//    }
+    /**
+     * 插入一个新节点
+     * 只需对最后一个插入的元素进行上浮操作即可
+     *
+     * @param arr
+     * @param value
+     * @return
+     */
+    public static int[] add(int[] arr, int value) {
+        int[] b = new int[arr.length+1];
+        b[arr.length] = value;
+        //System.out.println(b);
+        System.arraycopy(arr, 0,b,0, arr.length);
+        arr = b;
+
+        upHeap(arr);
+
+        return arr;
+    }
 
 
 }
