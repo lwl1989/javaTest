7879e9e87a3e5439a8f73c410a0cace7fc60dfc4
李文龙 <wenlong11@staff.weibo.com>
2018-09-27 11:36:33 +0800

    stack

---
+++ src/Algorithm/BagHeapStack/Stack.java
@@ -0,0 +1,38 @@
+package Algorithm.BagHeapStack;
+
+import java.util.ArrayList;
+
+//栈 LIFO(last input first out)  先进
+public class Stack <T> {
+    protected ArrayList values;
+    {
+        values = new ArrayList();
+    }
+
+    public void put(T value){
+        values.add(value);
+    }
+
+    public T get() {
+       return (T)values.remove(values.size() - 1);
+    }
+
+    public boolean hasNext()
+    {
+        return !values.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        char[] chars = {'{','[','{','}',']','(',')','}'};
+
+        Stack stack = new Stack();
+        for(char c : chars) {
+            stack.put(c);
+        }
+        char c;
+        while (stack.hasNext()) {
+            c = (char) (stack.get());
+            System.out.println(c);
+        }
+    }
+}
